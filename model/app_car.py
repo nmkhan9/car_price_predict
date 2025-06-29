6ea3df627e7bc01a8a67a4e0580646ee992fe2c1
khang <nguyenmanhkhanghd@gmail.com>
2025-06-29 16:18:52 +0700

    change app.py

--- model/app_car.py
+++ model/app_car.py
@@ -5,14 +5,13 @@ import numpy as np
 
 app = Flask(__name__)
 
-
-ridge_model = load("E:\Pycode\Project_code\ML_git\Car_price_prediction\model\ridge_model_1.joblib")
-ohe = load("E:\Pycode\Project_code\ML_git\Car_price_prediction\model\onehot_encoder.pkl")
-scaler = load("E:\Pycode\Project_code\ML_git\Car_price_prediction\model\scaler.pkl")
+ridge_model = load("E:\\Pycode\\Project_code\\ML_git\\Car_price_prediction\\model\\ridge_model_1.joblib")
+ohe = load("E:\\Pycode\\Project_code\\ML_git\\Car_price_prediction\\model\\onehot_encoder.pkl")
+scaler = load("E:\\Pycode\\Project_code\\ML_git\\Car_price_prediction\\model\\scaler.pkl")
 
 
 categorical_cols = ['Body_Type', 'Origin', 'Province', 'District', 'Transmission', 'Fuel_Type', 'Brand']
-numerical_cols = ['age', 'mileage_num']
+numerical_cols = ['age', 'mileage_num']  
 
 @app.route('/')
 def home():
@@ -21,7 +20,7 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-
+ 
         data = {
             'Body_Type': request.form['Body_Type'],
             'Origin': request.form['Origin'],
@@ -34,20 +33,30 @@ def predict():
             'mileage_num': float(request.form['mileage_num'])
         }
 
-
         input_df = pd.DataFrame([data])
+
+     
+        input_df = input_df[numerical_cols + categorical_cols] 
 
 
         encoded_categorical = ohe.transform(input_df[categorical_cols])
         encoded_categorical_df = pd.DataFrame(
             encoded_categorical,
-            columns=ohe.get_feature_names_out(categorical_cols)
+            columns=ohe.get_feature_names_out(categorical_cols),
+            index=input_df.index
         )
 
+      
         scaled_numerical = scaler.transform(input_df[numerical_cols])
-        scaled_numerical_df = pd.DataFrame(scaled_numerical, columns=numerical_cols)
+        scaled_numerical_df = pd.DataFrame(
+            scaled_numerical,
+            columns=numerical_cols,
+            index=input_df.index
+        )
 
-        processed_data = pd.concat([encoded_categorical_df, scaled_numerical_df], axis=1)
+   
+        processed_data = pd.concat([scaled_numerical_df, encoded_categorical_df], axis=1)
+
 
         prediction = ridge_model.predict(processed_data)[0]
 
