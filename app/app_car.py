6e56918a5f98c17ada34995e5f3a0aacff914869
khang <nguyenmanhkhanghd@gmail.com>
2025-07-29 23:35:27 +0700

    update file app deploy

--- app/app_car.py
+++ app/app_car.py
@@ -77,5 +77,7 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
